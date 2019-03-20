6ae35ab3a4928a58da3b59cde725cdaa8b0db8c7
HowellYang <th15817161961@gmail.com>
2019-03-20 12:02:55 +0800

    init

--- LoadBalance/nginx-main/buils.sh
+++ LoadBalance/nginx-main/buils.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-docker build -t nginx-main .
+docker build -t atomscat-nginx-main .
+# 启动
+#  docker run --name atomscat-nginx-main --link=backend-service1:nginx-backend-service1 --link=backend-service2:nginx-backend-service2 -p 8030:8030 -d atomscat-nginx-main
