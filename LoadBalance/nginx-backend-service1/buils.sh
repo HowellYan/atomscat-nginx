15e59b6bb85fcf66764724ee0e6621fed437d2d7
HowellYang <th15817161961@gmail.com>
2019-03-20 13:50:30 +0800

    init

--- LoadBalance/nginx-backend-service1/buils.sh
+++ LoadBalance/nginx-backend-service1/buils.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 docker build -t nginx-backend-service1 .
+# 启动
+#  docker run --name nginx-backend-service1 -p 8081:8080 -d nginx-backend-service1
--- LoadBalance/nginx-backend-service2/buils.sh
+++ LoadBalance/nginx-backend-service2/buils.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 docker build -t nginx-backend-service2 .
+# 启动
+#  docker run --name nginx-backend-service2 -p 8082:8080 -d nginx-backend-service2
